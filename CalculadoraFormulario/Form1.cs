6372fcb358afe54aaa367137d3822e407da9d4e6
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2023-07-31 16:38:32 -0300

    Primeiro Commit

---
+++ CalculadoraFormulario/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraFormulario
+{
+    public partial class Form1 : Form
+    {
+        ModelCalculadora modelo;
+        //Conectando a Control com a Modelo
+        public Form1()
+        {
+            InitializeComponent();
+            this.modelo = new ModelCalculadora();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.Somar(Convert.ToDouble(maskedTextBox1.Text));
+        }//fim do botão somar
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "" + this.modelo.Subtrair(Convert.ToDouble(maskedTextBox1.Text));
+        }//fim do botão subtrair
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CalculadoraFormulario/ModelCalculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraFormulario
+{
+    class ModelCalculadora
+    {
+        private double operacao;
+        public ModelCalculadora()
+        {
+            ConsultarOperacao = 0;
+        }//fim do construtor
+
+        public double ConsultarOperacao
+        {
+            get { return operacao; }
+            set { this.operacao = value; }
+        }//fim do método
+        public double Somar(double num)
+        {
+            return ConsultarOperacao += num;
+        }//fim do somar
+
+        public double Subtrair(double num)
+        {
+            return ConsultarOperacao -= num;
+        }//fim do somar
+    }//fim da classe
+}//fim do projeto 
